1531fe34a63fb9dceff8bd2c8ced8fd39bb44830
sjnscythe <scytheabhi97@gmail.com>
2023-04-04 12:34:57 +0530

    Initial commit

---
+++ Example/React-Codegen/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  React-Codegen
+//
+//  Created by sjnscythe on 04/04/2023.
+//  Copyright (c) 2023 sjnscythe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
